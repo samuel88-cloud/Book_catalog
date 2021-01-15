d08aaef641d53f5b61c321f0e80f2ea9198934f5
samuel stephen <samuel8stephen@gmail.com>
2021-01-15 13:52:30 +0530

    added try catch block to run.py

--- run.py
+++ run.py
@@ -16,6 +16,8 @@ if __name__ == '__main__':
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
-flask_app.run()
+    try:
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry', email='harry@potters.com', password='secret')
+    except exc.IntegrityError:
+        flask_app.run()
