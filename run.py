58716cc5aea1f0cec3a437389996ce8d0ef68420
samuel stephen <samuel8stephen@gmail.com>
2021-01-15 13:22:30 +0530

    changed db and run configurations

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app, db  # app/__init__ (ROOT PACKAGE)
 from app.auth.models import User
 
+#dev
+'''
 if __name__ == '__main__':
     flask_app = create_app('prod')
     with flask_app.app_context():
@@ -8,3 +10,12 @@ if __name__ == '__main__':
         if not User.query.filter_by(user_name='harry').first():
             User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
     flask_app.run()
+'''
+
+#prod
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    if not User.query.filter_by(user_name='harry').first():
+        User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
+flask_app.run()
